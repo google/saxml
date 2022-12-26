4ff82c8ba3fe2ee9b805e0948ce0e904cc31da5f
Yuanzhong Xu <yuanzx@google.com>
2022-12-25 19:01:14 -0800

    Streaming decoding with sentencepiece
    PiperOrigin-RevId: 497716084
    Change-Id: Ib3664c14ce1b8b691b259962f9f055b0d555bbbb

--- saxml/server/pax/lm/lm_tokenizer.py
+++ saxml/server/pax/lm/lm_tokenizer.py
@@ -36,17 +36,22 @@ class LMTokenizer(base_hyperparams.BaseParameterizable):
       target_eos_id: End of sentence id.
       slice_left: Slice the left part of the sequence if it is too long.
         Otherwise, slice the right part of the sequence.
+      streaming_whitespace_preserving_prefix: A prefix added to each non-SOS
+        streaming decoding step to prevent the leading whitespace from being
+        removed by sentencepiece; after decoding the step, it will be removed
+        from the string result. It must be a regular token in the vocabulary.
     """
     append_eos: bool = True
     spm_model: str = None
     target_sos_id: int = 0
     target_eos_id: int = 1
     slice_left: bool = True
+    streaming_whitespace_preserving_prefix: str = 'a'
 
   def __init__(self, hparams: LMTokenizer.HParams) -> None:
     super().__init__(hparams)
     assert hparams.append_eos
-    self._vocab = seqio.SentencePieceVocabulary(self._hparams.spm_model, 0)
+    self._vocab = seqio.SentencePieceVocabulary(self.hparams.spm_model, 0)
 
   def StringsToIds(self, strs: tf.Tensor,
                    max_length: int) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
@@ -64,7 +69,7 @@ class LMTokenizer(base_hyperparams.BaseParameterizable):
       - labels[i, j] is the target token id of i-th sample for j-th step.
       - paddings[i, j] is 1 iff i-th sample's j-th step is padded.
     """
-    p = self._hparams
+    p = self.hparams
     assert p.spm_model
     assert max_length is not None
     batch = tf.shape(strs)[0]
@@ -112,7 +117,7 @@ class LMTokenizer(base_hyperparams.BaseParameterizable):
 
     return ids, labels, paddings
 
-  def IdsToStrings(self, ids: List[List[int]], *unused_args: Any) -> tf.Tensor:
+  def IdsToStrings(self, ids: tf.Tensor, *unused_args: Any) -> tf.Tensor:
     """Converts ids back to strings.
 
     Decoding stops at padding or eos token.
@@ -125,6 +130,103 @@ class LMTokenizer(base_hyperparams.BaseParameterizable):
     Returns:
       sequences - A vector of shape [batch]. The converted string sequence.
     """
-    p = self._hparams
+    p = self.hparams
     assert p.spm_model
     return self._vocab.tf_tokenizer.detokenize(ids)
+
+  def InitStream(self, batch_size: int) -> Tuple[tf.RaggedTensor, tf.Tensor]:
+    """Create the initial state for streaming.
+
+    Args:
+      batch_size: the number of decoding streams.
+
+    Returns:
+
+      A tuple of 2 elements:
+        - A ragged tensor of [batch_size, seqlen] unprocessed prefix IDs.
+        - A boolean tensor [batch_size] indicating if any prefix strings have
+          been generated.
+    """
+    return tf.ragged.constant(
+        [[]] * batch_size, dtype=tf.int32), tf.fill([batch_size], False)
+
+  def DecodeOnStream(
+      self, stream_state: Tuple[tf.RaggedTensor, tf.Tensor], new_ids: tf.Tensor
+  ) -> Tuple[tf.Tensor, Tuple[tf.RaggedTensor, tf.Tensor]]:
+    """Converts new chunks of IDs on decoding streams.
+
+    Args:
+      stream_state: stream state. See description in InitStream.
+      new_ids: A matrix of shape [batch, new_chunk_len] for the newly generated
+        IDs for streaming.
+
+    Returns:
+      A tuple of (newly decoded strings, updated stream state)
+    """
+    unprocessed_prefix_ids, started = stream_state
+    p = self.hparams
+    assert p.spm_model
+    # Find the byte-encoded IDs.
+    new_ids_shape = tf.shape(new_ids)
+    b, new_seqlen = new_ids_shape[0], new_ids_shape[1]
+    new_pieces = self._vocab.tf_tokenizer.id_to_string(new_ids)
+    is_byte = tf.strings.regex_full_match(new_pieces, '<0x[0-9,A-F][0-9,A-F]>$')
+    # Remove trailing bytes.
+    trailing_byte_count = tf.reduce_sum(
+        tf.cast(
+            tf.equal(
+                tf.cumsum(1 - tf.cast(is_byte, tf.int32), axis=1, reverse=True),
+                0), tf.int32),
+        axis=1)
+    without_trailing_bytes = tf.RaggedTensor.from_tensor(
+        new_ids, new_seqlen - trailing_byte_count)
+    is_all_bytes = tf.equal(trailing_byte_count, new_seqlen)
+
+    # Add a fake prefix to preserve leading whitespace if earlier prefix was
+    # generated.
+    fake_prefix_str = p.streaming_whitespace_preserving_prefix
+    fake_prefix = self._vocab.tf_tokenizer.tokenize([fake_prefix_str
+                                                    ]).to_tensor()
+    fake_prefix = tf.repeat(fake_prefix, b, axis=0)
+    fake_prefix_len = tf.fill([b], tf.shape(fake_prefix)[1])
+    fake_prefix_str_len = tf.fill([b],
+                                  tf.constant(
+                                      len(fake_prefix_str), dtype=tf.int32))
+    fake_prefix_len = tf.where(started, fake_prefix_len, 0)
+    fake_prefix_str_len = tf.where(started, fake_prefix_str_len, 0)
+    fake_prefix = tf.RaggedTensor.from_tensor(fake_prefix, fake_prefix_len)
+
+    # Decode with prefix.
+    to_process = tf.concat(
+        [fake_prefix, unprocessed_prefix_ids, without_trailing_bytes], axis=1)
+    new_strs = self._vocab.tf_tokenizer.detokenize(to_process)
+    # Remove fake prefix.
+    new_strs = tf.strings.substr(new_strs, fake_prefix_str_len, tf.fill([b],
+                                                                        -1))
+    new_strs = tf.where(is_all_bytes, tf.constant(''), new_strs)
+
+    new_started = tf.logical_or(started, tf.strings.length(new_strs) > 0)
+
+    trailing_bytes = tf.RaggedTensor.from_tensor(
+        tf.reverse(new_ids, axis=[1]), trailing_byte_count)
+    trailing_bytes = tf.reverse(trailing_bytes, axis=[1])
+
+    remaining_prefix_len = unprocessed_prefix_ids.row_lengths()
+    remaining_prefix_len = tf.where(is_all_bytes, remaining_prefix_len, 0)
+    remaining_prefix = tf.RaggedTensor.from_tensor(
+        unprocessed_prefix_ids.to_tensor(), remaining_prefix_len)
+    new_unprocessed_prefix_ids = tf.concat([remaining_prefix, trailing_bytes],
+                                           axis=1)
+
+    return new_strs, (new_unprocessed_prefix_ids, new_started)
+
+  def FinishStream(
+      self, stream_state: Tuple[tf.RaggedTensor, tf.Tensor]) -> tf.Tensor:
+    """Finishes the streams by decoding any remaining tokens."""
+    p = self.hparams
+    assert p.spm_model
+    b = tf.shape(stream_state[1])[0]
+    new_strs, _ = self.DecodeOnStream(
+        stream_state,
+        tf.fill([b, 1], tf.constant(p.target_eos_id, dtype=tf.int32)))
+    return new_strs
--- saxml/server/pax/lm/lm_tokenizer_test.py
+++ saxml/server/pax/lm/lm_tokenizer_test.py
@@ -16,15 +16,14 @@
 
 import os
 
+from absl import flags
 from absl.testing import parameterized
-from lingvo import compat as tf
-from lingvo.core import test_utils
 from praxis import base_layer
-
 from saxml.server.pax.lm import lm_tokenizer
+import tensorflow as tf
 
 instantiate = base_layer.instantiate
-FLAGS = tf.flags.FLAGS
+FLAGS = flags.FLAGS
 
 
 def _CreateParams():
@@ -38,7 +37,7 @@ def _CreateParams():
   return p
 
 
-class LMTokenizerTest(test_utils.TestCase, parameterized.TestCase):
+class LMTokenizerTest(tf.test.TestCase, parameterized.TestCase):
 
   def testStringsToIds(self):
     p = _CreateParams()
@@ -46,9 +45,6 @@ class LMTokenizerTest(test_utils.TestCase, parameterized.TestCase):
     max_length = 5
     strs = ['hello world', 'the quick brown fox jumps']
     ids, labels, paddings = tokenizer.StringsToIds(strs, max_length)
-
-    with self.session() as sess:
-      ids, labels, paddings = sess.run([ids, labels, paddings])
     self.assertAllEqual([[0, 9873, 640, 0, 0], [0, 261, 1242, 3350, 9806]], ids)
     self.assertAllEqual(
         [[9873, 640, 1, 0, 0], [261, 1242, 3350, 9806, 1]], labels)
@@ -61,9 +57,6 @@ class LMTokenizerTest(test_utils.TestCase, parameterized.TestCase):
     max_length = 8
     strs = ['hello world', 'the quick brown fox jumps']
     ids, labels, paddings = tokenizer.StringsToIds(strs, max_length)
-
-    with self.session() as sess:
-      ids, labels, paddings = sess.run([ids, labels, paddings])
     self.assertAllEqual([[0, 9873, 640, 0, 0, 0, 0, 0],
                          [0, 261, 1242, 3350, 9806, 11144, 0, 0]], ids)
     self.assertAllEqual([[9873, 640, 1, 0, 0, 0, 0, 0],
@@ -77,9 +70,6 @@ class LMTokenizerTest(test_utils.TestCase, parameterized.TestCase):
     max_length = 4
     strs = ['hello world', 'the quick brown fox jumps']
     ids, labels, paddings = tokenizer.StringsToIds(strs, max_length)
-
-    with self.session() as sess:
-      ids, labels, paddings = sess.run([ids, labels, paddings])
     self.assertAllEqual([[0, 9873, 640, 0], [0, 261, 1242, 3350]], ids)
     self.assertAllEqual([[9873, 640, 1, 0], [261, 1242, 3350, 1]], labels)
     self.assertAllEqual([[0.0, 0.0, 0.0, 1.0], [0.0, 0.0, 0.0, 0.0]], paddings)
@@ -91,9 +81,6 @@ class LMTokenizerTest(test_utils.TestCase, parameterized.TestCase):
     max_length = 4
     strs = ['hello world', 'the quick brown fox jumps']
     ids, labels, paddings = tokenizer.StringsToIds(strs, max_length)
-
-    with self.session() as sess:
-      ids, labels, paddings = sess.run([ids, labels, paddings])
     self.assertAllEqual([[0, 9873, 640, 0], [0, 3350, 9806, 11144]], ids)
     self.assertAllEqual([[9873, 640, 1, 0], [3350, 9806, 11144, 1]], labels)
     self.assertAllEqual([[0.0, 0.0, 0.0, 1.0], [0.0, 0.0, 0.0, 0.0]], paddings)
@@ -104,10 +91,46 @@ class LMTokenizerTest(test_utils.TestCase, parameterized.TestCase):
     ids = [[9873, 640, 1, 0, 0, 0, 0, 0, 0, 0],
            [261, 1242, 3350, 9806, 11144, 1, 0, 0, 0, 0]]
     strs = tokenizer.IdsToStrings(ids)
-    with self.session() as sess:
-      strs = sess.run(strs)
-
     self.assertEqual([b'hello world', b'the quick brown fox jumps'], list(strs))
+
+  def testStreamWhiteSpaces(self):
+    p = _CreateParams()
+    tokenizer = instantiate(p)
+    state = tokenizer.InitStream(2)
+
+    strs, state = tokenizer.DecodeOnStream(state, [[0], [261]])
+    self.assertEqual([b'', b'the'], list(strs))
+    strs, state = tokenizer.DecodeOnStream(state, [[9873], [1242]])
+    self.assertEqual([b'hello', b' quick'], list(strs))
+    strs, state = tokenizer.DecodeOnStream(state,
+                                           [[640, 1, 0], [3350, 9806, 11144]])
+    self.assertEqual([b' world', b' brown fox jumps'], list(strs))
+    strs, state = tokenizer.DecodeOnStream(state, [[0], [1]])
+    self.assertEqual([b'', b''], list(strs))
+    strs = tokenizer.FinishStream(state)
+    self.assertEqual([b'', b''], list(strs))
+
+  def testStreamBytes(self):
+    p = _CreateParams()
+    tokenizer = instantiate(p)
+    byte_ids = list(
+        tokenizer._vocab.tf_tokenizer.string_to_id(
+            #   g           h           i
+            [b'<0x67>', b'<0x68>', b'<0x69>']))
+    state = tokenizer.InitStream(2)
+
+    strs, state = tokenizer.DecodeOnStream(state, [[9873], [261]])
+    self.assertEqual([b'hello', b'the'], list(strs))
+    strs, state = tokenizer.DecodeOnStream(state,
+                                           [[byte_ids[0]], [byte_ids[1]]])
+    self.assertEqual([b'', b''], list(strs))
+    strs, state = tokenizer.DecodeOnStream(state, [[byte_ids[2]], [1242]])
+    self.assertEqual([b'', b'h quick'], list(strs))
+    strs, state = tokenizer.DecodeOnStream(
+        state, [[byte_ids[2], 640], [byte_ids[0], byte_ids[1]]])
+    self.assertEqual([b'gii world', b''], list(strs))
+    strs = tokenizer.FinishStream(state)
+    self.assertEqual([b'', b'gh'], list(strs))
 
 
 if __name__ == '__main__':
