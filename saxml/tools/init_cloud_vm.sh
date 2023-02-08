21d6f38070a08cb817d06669a5abcb8fe0b1a9fb
Sax Authors <no-reply@google.com>
2023-02-08 09:50:19 -0800

    Upgrade pip first to avoid pip install errors
    PiperOrigin-RevId: 508106999
    Change-Id: I359bac556dc2cf094af848c57e2339b11b87c9c4

--- saxml/tools/init_cloud_vm.sh
+++ saxml/tools/init_cloud_vm.sh
@@ -22,4 +22,5 @@ curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key --keyring /usr/sha
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 sudo apt -y update && sudo apt install -y bazel
 
+pip3 install -U pip
 pip3 install numpy  # @org_tensorflow requires numpy installation in system
