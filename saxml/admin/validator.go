d0a56ae6cb6e9aa6c572d5bc9ff831590d1bcfe7
Sax Authors <no-reply@google.com>
2023-03-23 15:18:51 -0700

    Allows `|` in checkpoint path.
    PiperOrigin-RevId: 518976560
    Change-Id: I5da619cf96243a3b368dead936c3c77e6db98762

--- saxml/admin/validator.go
+++ saxml/admin/validator.go
@@ -37,7 +37,7 @@ const (
 	modelPathPattern = "^[a-zA-Z]+[a-zA-Z0-9_.-]*$"
 
 	// Valid checkpoint pattern.
-	checkpointPattern = `^(/|\d|\w|_|\.|-|%|:|=)+$`
+	checkpointPattern = `^(/|\d|\w|_|\.|-|%|:|=|\|)+$`
 )
 
 var (
