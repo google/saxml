b83bff8cfc22ee258d892c9c23f05e9d0b72bd8d
Chang Lan <changlan@google.com>
2023-04-13 14:13:15 -0700

    Improve error msg readability
    PiperOrigin-RevId: 524099635
    Change-Id: I5286538ab28f5f7a7c66f95a9f5ff67eec8de088

--- saxml/server/validate.py
+++ saxml/server/validate.py
@@ -61,17 +61,17 @@ def ValidateRequestForExtraInputs(
   for input_key in req_extra_inputs:
     if extra_inputs is None or input_key not in extra_inputs:
       return utils.invalid_arg(
-          f"key {input_key} in RPC request's extra_inputs field is not in "
-          'ServableModel.extra_inputs.extra_inputs in ServableModel are '
-          f'{extra_inputs}'
+          f'The extra_inputs key `{input_key}` in the RPC request is not'
+          ' defined in ServableModel.extra_inputs. Current'
+          f' ServableModel.extra_inputs is {extra_inputs}.'
       )
 
   for key, tensor in dict(req.extra_inputs.tensors).items():
     if extra_inputs is None or key not in extra_inputs:
       return utils.invalid_arg(
-          f"key {key} in RPC request's extra_inputs field is not in "
-          'ServableModel.extra_inputs.extra_inputs in ServableModel are '
-          f'{extra_inputs}'
+          f'The extra_inputs key `{key}` in the RPC request is not'
+          ' defined in ServableModel.extra_inputs. Current'
+          f' ServableModel.extra_inputs is {extra_inputs}.'
       )
     if key in req_extra_inputs:
       return utils.invalid_arg(
