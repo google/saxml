1116f0f17c630d6b300decbaa0dba6866d41e88c
Sax Authors <no-reply@google.com>
2023-05-15 19:43:28 -0700

    Added missing 'GetTimeout' to the python interface.
    PiperOrigin-RevId: 532300301
    Change-Id: I244514c118602694c5a36634e94e10c5102b00d0

--- saxml/client/python/sax.cc
+++ saxml/client/python/sax.cc
@@ -48,6 +48,7 @@ PYBIND11_MODULE(sax, m) {
       .def("SetExtraInput", &sax::client::ModelOptions::SetExtraInput)
       .def("SetExtraInputTensor",
            &sax::client::ModelOptions::SetExtraInputTensor)
+      .def("GetTimeout", &sax::client::ModelOptions::GetTimeout)
       .def("SetTimeout", &sax::client::ModelOptions::SetTimeout)
       .def("ToDebugString", [](sax::client::ModelOptions& mo) {
         ::sax::ExtraInputs extra_inputs;
