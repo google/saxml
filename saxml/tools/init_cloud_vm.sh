91f101fdea0ce975fbc9b71c57eaf193f69c9350
Sax Authors <no-reply@google.com>
2024-01-31 15:54:40 -0800

    fix init_cloud_vm.sh bug
    PiperOrigin-RevId: 603184455
    Change-Id: Ic182b5fd6a53fa7ff8ec2726b65e861de99386f8

--- saxml/tools/init_cloud_vm.sh
+++ saxml/tools/init_cloud_vm.sh
@@ -20,8 +20,8 @@ sudo apt -y update && sudo apt install -y apt-transport-https curl gnupg patch p
 
 curl https://bazel.build/bazel-release.pub.gpg | apt-key --keyring /usr/share/keyrings/bazel-archive-keyring.gpg add -
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-RUN apt -y update && apt install -y bazel-6.4.0
-RUN update-alternatives --install /usr/bin/bazel bazel /usr/bin/bazel-6.4.0 20
+sudo apt -y update && apt install -y bazel-6.4.0
+sudo update-alternatives --install /usr/bin/bazel bazel /usr/bin/bazel-6.4.0 20
 
 pip3 install -U pip
 pip3 install numpy  # @org_tensorflow requires numpy installation in system
