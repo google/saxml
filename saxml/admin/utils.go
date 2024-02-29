2b08ed420db487464be56101ec3a0a2bb95505ab
Sax Authors <no-reply@google.com>
2024-02-29 10:55:30 -0800

    add admin/utils.go
    PiperOrigin-RevId: 611527337
    Change-Id: I3da78c461c8a09234cc9bbfef3085c0311698134

---
+++ saxml/admin/utils.go
@@ -0,0 +1,141 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package utils provides helper routines for the admin server implementation.
+package utils
+
+import (
+	"strconv"
+
+	log "github.com/golang/glog"
+	"saxml/admin/protobuf"
+	apb "saxml/protobuf/admin_go_proto_grpc"
+)
+
+// GetServerMemoryCapacity estimates the model server's memory capacity (in bytes).
+func GetServerMemoryCapacity(spec *protobuf.ModelServer) int64 {
+	var memGBPerCore int64
+	switch apb.ModelServer_ChipType(spec.ChipType) {
+	case apb.ModelServer_CHIP_TYPE_UNKNOWN:
+		memGBPerCore = 8 // Assumption
+	case apb.ModelServer_CHIP_TYPE_TPU_V2:
+		memGBPerCore = 8
+	case apb.ModelServer_CHIP_TYPE_TPU_V3:
+		memGBPerCore = 16
+	case apb.ModelServer_CHIP_TYPE_TPU_V4:
+		memGBPerCore = 16
+	case apb.ModelServer_CHIP_TYPE_TPU_V4I:
+		memGBPerCore = 8
+	case apb.ModelServer_CHIP_TYPE_TPU_V5:
+		memGBPerCore = 48
+	case apb.ModelServer_CHIP_TYPE_TPU_V5I:
+		memGBPerCore = 16
+	case apb.ModelServer_CHIP_TYPE_TPU_V5E:
+		memGBPerCore = 16
+	case apb.ModelServer_CHIP_TYPE_GPU_P100:
+		memGBPerCore = 16
+	case apb.ModelServer_CHIP_TYPE_GPU_V100:
+		memGBPerCore = 16
+	case apb.ModelServer_CHIP_TYPE_GPU_T4:
+		memGBPerCore = 16
+	case apb.ModelServer_CHIP_TYPE_GPU_A100:
+		memGBPerCore = 40
+	case apb.ModelServer_CHIP_TYPE_GPU_H100:
+		memGBPerCore = 80
+	case apb.ModelServer_CHIP_TYPE_GPU_L4:
+		memGBPerCore = 24
+	case apb.ModelServer_CHIP_TYPE_CPU:
+		memGBPerCore = 64 // Assumption
+	}
+	var numCores int64
+	switch apb.ModelServer_ChipTopology(spec.ChipTopology) {
+	case apb.ModelServer_CHIP_TOPOLOGY_UNKNOWN:
+		numCores = 1 // Assumption
+	case apb.ModelServer_CHIP_TOPOLOGY_1:
+		numCores = 1
+	case apb.ModelServer_CHIP_TOPOLOGY_2:
+		numCores = 2
+	case apb.ModelServer_CHIP_TOPOLOGY_4:
+		numCores = 4
+	case apb.ModelServer_CHIP_TOPOLOGY_8:
+		numCores = 8
+	case apb.ModelServer_CHIP_TOPOLOGY_16:
+		numCores = 16
+	case apb.ModelServer_CHIP_TOPOLOGY_1X1:
+		numCores = 1
+	case apb.ModelServer_CHIP_TOPOLOGY_2X2:
+		numCores = 4
+	case apb.ModelServer_CHIP_TOPOLOGY_2X4:
+		numCores = 8
+	case apb.ModelServer_CHIP_TOPOLOGY_4X2:
+		numCores = 8
+	case apb.ModelServer_CHIP_TOPOLOGY_4X4:
+		numCores = 16
+	case apb.ModelServer_CHIP_TOPOLOGY_4X8:
+		numCores = 32
+	case apb.ModelServer_CHIP_TOPOLOGY_8X8:
+		numCores = 64
+	case apb.ModelServer_CHIP_TOPOLOGY_8X16:
+		numCores = 128
+	case apb.ModelServer_CHIP_TOPOLOGY_16X16:
+		numCores = 256
+	case apb.ModelServer_CHIP_TOPOLOGY_16X32:
+		numCores = 512
+	case apb.ModelServer_CHIP_TOPOLOGY_32X32:
+		numCores = 1024
+	case apb.ModelServer_CHIP_TOPOLOGY_1X1X1:
+		numCores = 1
+	case apb.ModelServer_CHIP_TOPOLOGY_1X2X1:
+		numCores = 2
+	case apb.ModelServer_CHIP_TOPOLOGY_2X2X1:
+		numCores = 4
+	case apb.ModelServer_CHIP_TOPOLOGY_2X2X2:
+		numCores = 8
+	case apb.ModelServer_CHIP_TOPOLOGY_2X2X4:
+		numCores = 16
+	case apb.ModelServer_CHIP_TOPOLOGY_2X4X4:
+		numCores = 32
+	case apb.ModelServer_CHIP_TOPOLOGY_4X4X4:
+		numCores = 64
+	case apb.ModelServer_CHIP_TOPOLOGY_4X4X8:
+		numCores = 128
+	case apb.ModelServer_CHIP_TOPOLOGY_4X4X16:
+		numCores = 256
+	case apb.ModelServer_CHIP_TOPOLOGY_4X8X8:
+		numCores = 256
+	case apb.ModelServer_CHIP_TOPOLOGY_8X8X12:
+		numCores = 768
+	case apb.ModelServer_CHIP_TOPOLOGY_8X8X8:
+		numCores = 512
+	}
+	return memGBPerCore * numCores * (1 << 30)
+}
+
+// GetMemoryRequired estimates the model's memory usage based on
+// spec.overrided["ram"]. Returns <0 if the estimation is not
+// feasible.
+func GetMemoryRequired(spec *apb.Model) int64 {
+	// TODO: Maybe define a field in apb.Model. But for now, we expect
+	// a field in the override provides a hint.
+	found, ok := spec.GetOverrides()["ram"]
+	if !ok {
+		return -1
+	}
+	value, err := strconv.ParseInt(found, 10, 64)
+	if err != nil {
+		log.Errorf("Invalid number found in \"ram\": %s", found)
+		return -1
+	}
+	return value
+}
---
+++ saxml/admin/utils_test.go
@@ -0,0 +1,74 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package utils
+
+import (
+	"testing"
+
+	"saxml/admin/protobuf"
+	apb "saxml/protobuf/admin_go_proto_grpc"
+)
+
+func TestGetServerMemoryCapacity(t *testing.T) {
+	tests := []struct {
+		chipType   apb.ModelServer_ChipType
+		chipTopo   apb.ModelServer_ChipTopology
+		expectedGB int64
+	}{
+		{
+			apb.ModelServer_CHIP_TYPE_TPU_V5I,
+			apb.ModelServer_CHIP_TOPOLOGY_1,
+			16,
+		},
+		{
+			apb.ModelServer_CHIP_TYPE_TPU_V5,
+			apb.ModelServer_CHIP_TOPOLOGY_4X4X8,
+			48 * 128,
+		},
+	}
+	for _, tc := range tests {
+		t.Run("server mem", func(t *testing.T) {
+			ms := protobuf.ModelServer{
+				ChipType:     protobuf.ChipType(tc.chipType),
+				ChipTopology: protobuf.ChipTopology(tc.chipTopo),
+			}
+			actual := GetServerMemoryCapacity(&ms)
+			if actual != tc.expectedGB*(1<<30) {
+				t.Errorf("GetServerMemoryCapacity(%v) err got %d, want %d", ms, actual, tc.expectedGB)
+			}
+		})
+	}
+}
+
+func TestGetMemoryRequired(t *testing.T) {
+	tests := []struct {
+		ram      string
+		expected int64
+	}{
+		{"", -1},
+		{"12345678", 12345678},
+		{"random blah", -1},
+	}
+	for _, tc := range tests {
+		spec := &apb.Model{}
+		if tc.ram != "" {
+			spec.Overrides = map[string]string{"ram": tc.ram}
+		}
+		actual := GetMemoryRequired(spec)
+		if actual != tc.expected {
+			t.Errorf("GetMemoryRequired(%v) err got %d, want %d", spec, actual, tc.expected)
+		}
+	}
+}
